5c4d7d0651a1ab796e9312dfd004cd180806400d
James Declerus <james.declerus@esih.edu>
2019-05-23 14:30:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,8 @@
-
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("John", 11, 3.4, 3);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Kareem", 10, 2.4, 12);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Zak", 12, 4.0, 0);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Kim", 11, 2.0, 9);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Bob", 10, 1.9, 17);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Stef", 9, 2.5, 2);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("George", 9, 3.9, 1);
-    INSERT INTO students (name, grade, gpa, tardies)
-        VALUES ("Daryl", 9, 3.5, 5);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "black", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "M", "green", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 18, "F", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 22, "M", "white", "kind", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "F", "blue", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 19, "M", "brown", "loving", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 11, "M", "white", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 15, "M", "pink", "kind", 0);
